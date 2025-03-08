4a7c7469485f6e246bc48581e68ae70a5ada0dc5
dermipa <mipa2086@gmail.com>
2025-03-08 01:22:04 +0100

    new

--- src/main/java/de/mipa/lobby/Listener/ProtectionListener.java
+++ src/main/java/de/mipa/lobby/Listener/ProtectionListener.java
@@ -1,6 +1,8 @@
 package de.mipa.lobby.Listener;
 
 import de.mipa.lobby.Main;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +10,9 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.block.Action;
 
 public class ProtectionListener implements Listener {
 
@@ -17,7 +22,7 @@ public class ProtectionListener implements Listener {
     @EventHandler
     public void onBlockBreak(final BlockBreakEvent event) {
         Player player = event.getPlayer();
-        if (!player.hasPermission("lobby.build")){
+        if (!player.hasPermission("lobby.build")) {
             event.setCancelled(true);
         }
     }
@@ -25,22 +30,44 @@ public class ProtectionListener implements Listener {
     @EventHandler
     public void onBlockPlace(final BlockPlaceEvent event) {
         Player player = event.getPlayer();
-        if (!player.hasPermission("lobby.build")){
+        if (!player.hasPermission("lobby.build")) {
             event.setCancelled(true);
-
         }
     }
 
     @EventHandler
     public void onPlayerDropItem(final PlayerDropItemEvent event) {
         Player player = event.getPlayer();
-        if (!player.hasPermission("lobby.build")){
+        if (!player.hasPermission("lobby.build")) {
             event.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void onPlayerHunger(FoodLevelChangeEvent event){
+    public void onPlayerHunger(FoodLevelChangeEvent event) {
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onSignInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+
+        // Prüfen, ob der Spieler ein Schild rechtsklickt
+        if (block != null && (block.getState() instanceof Sign)) {
+            if (!player.hasPermission("lobby.sign.edit") && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                event.setCancelled(true); // Schild-Editor blockieren
+            }
+        }
+    }
+
+    @EventHandler
+    public void onUnderwaterBreath(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+
+        // Falls der Spieler unter Wasser ist, setze seine Luft auf Maximum
+        if (player.isInWater()) {
+            player.setRemainingAir(player.getMaximumAir());
+        }
     }
 }
